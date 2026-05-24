6ec8bcf80841649b703fcb966910c14244593586
Takemichi_R7 <166553899+JoseIvanR7@users.noreply.github.com>
2026-05-24 14:00:28 -0600

    se agregaron cosas visuales, total de tablas y almacenamiento. Además de una actualización automática de 5 seg

--- Paginaweb/reportes_db.php
+++ Paginaweb/reportes_db.php
@@ -19,6 +19,30 @@ WHERE table_schema = 'SGR'
 $result = $conn->query($query);
 
 
+$totalTablas = 0;
+
+if ($result) {
+
+    $totalTablas = $result->num_rows;
+}
+
+
+$queryStorage = "
+SELECT ROUND (SUM(data_length + index_length)/1024,2) AS TotalKB
+FROM information_schema.tables
+WHERE table_schema = 'SGR'
+";
+
+$storageResult = $conn->query($queryStorage);
+
+$totalKB = 0;
+
+if ($storageResult) {
+
+    $filaStorage = $storageResult->fetch_assoc();
+    $totalKB = $filaStorage['TotalKB'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +53,8 @@ $result = $conn->query($query);
 
 <link rel="stylesheet"
 href="https://www.w3schools.com/w3css/4/w3.css">
+
+<meta http-equiv="refresh" content="5">
 
 </head>
 
@@ -45,6 +71,48 @@ href="https://www.w3schools.com/w3css/4/w3.css">
             Regresar al Dashboard
         </a>
 
+<div class="w3-row-padding
+            w3-margin-bottom">
+
+    <div class="w3-half">
+
+        <div class="w3-card
+                    w3-green
+                    w3-padding">
+
+            <h3>Total de tablas</h3>
+
+            <p>
+                <?php
+                echo $totalTablas;
+                ?>
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="w3-half">
+
+        <div class="w3-card
+                    w3-orange
+                    w3-padding">
+
+            <h3>Almacenamiento</h3>
+
+            <p>
+                <?php
+                echo $totalKB;
+                ?>
+                KB
+            </p>
+
+        </div>
+
+    </div>
+
+</div>
+
 <tr class="w3-blue">
 
 <th>Tabla</th>
@@ -52,6 +120,7 @@ href="https://www.w3schools.com/w3css/4/w3.css">
 <th>Datos</th>
 <th>Tamaño KB</th>
 <th>Fecha Creación</th>
+<th>Estado</th>
 
 </tr>
 
@@ -61,6 +130,14 @@ if ($result && $result->num_rows > 0) {
 
     while($fila = $result->fetch_assoc()) {
 
+        $estado = "NORMAL";
+        $claseEstado = "w3-green";
+
+        if ($fila['Datos'] == 1000) {
+            $estado = "ALTO USO";
+            $claseEstado = "w3-red";
+        }
+
         echo "
         <tr>
 
@@ -69,6 +146,7 @@ if ($result && $result->num_rows > 0) {
             <td>{$fila['Datos']}</td>
             <td>{$fila['TamañoKB']}</td>
             <td>{$fila['Creacion']}</td>
+            <td><span class='w3-tag($claseEstado)'>{$estado}</span></td>
 
         </tr>
         ";
@@ -89,7 +167,20 @@ if ($result && $result->num_rows > 0) {
 
 </table>
 
+<br>
+
+<div class="w3-panel w3-pale-blue w3-leftbar w3-border-blue">
+
+    <p>
+        <b>Observacion</b>
+        La información presentada corresponde al monitoreo estructural de 
+        la base de datos SGR.
+    </p>
+
+</div>
+
 </div>
 
 </body>
+
 </html>
